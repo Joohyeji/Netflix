030709a16d61d5b4b00a4d3c223170f69c40682d
Joohyeji <rosyjoo1999@gmail.com>
2024-02-07 15:20:16 +0900

    기초 이해하기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import Button from './Button';
+import styles from './App.module.css';
 
-import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev)=>prev+1);
+  console.log('render');
   return (
     <div className="App">
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click</button>
     </div>
   );
 }
