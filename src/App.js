607f923167b774c09d6d87a1909633d81155d1c0
Joohyeji <rosyjoo1999@gmail.com>
2024-02-21 17:13:45 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   const [counter, setValue] = useState(0);
-  const onClick = () => setValue((prev)=>prev+1);
+  const [keyword, setKeyword] = useState('');
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (e) => setKeyword(e.target.value);
   console.log('render');
-  const iRunOnlyOnce = () => {
-    console.log('run only once');
-  }
-  useEffect(() => {},[]);
+  useEffect(() => {
+    console.log('Call the API....')
+  }, []); //딱 한 번 실행
+  useEffect(() => {
+    if(keyword !== '' && keyword.length > 5)
+    console.log('SEARCH FOR',keyword)
+  }, [keyword]); //keyword가 변할 때 마다 실행
   return (
-    <div className="App">
+    <div>
+      <input
+        onChange={onChange}
+        value={keyword}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>click</button>
     </div>
